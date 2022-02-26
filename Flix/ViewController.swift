aba321ad2255ed40706b8a381a5b852cea23c9a5
Han094 <55882538+Han094@users.noreply.github.com>
2022-02-26 00:43:24 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by weihan on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
